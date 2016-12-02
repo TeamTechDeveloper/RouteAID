dccc4980bb1039fddf8da9b914c4cfd44b0fd169
vlaxb <vladix88@gmail.com>
2016-12-01 22:01:52 -0500

    Login+intro
    intro comprueba internet, login primera plantilla

---
+++ app/src/main/java/com/example/user/routeaid/Login.java
@@ -0,0 +1,17 @@
+package com.example.user.routeaid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 01/12/2016.
+ */
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
